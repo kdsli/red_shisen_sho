6ff773f80d23656ad97042c82c8365e499834daf
kdsli <kdsli@kdsli.ru>
2019-05-30 23:23:24 +0300

    Developing

--- cmake/get_version.cmake
+++ cmake/get_version.cmake
@@ -12,7 +12,8 @@ function(getVersion version date)
             set(PROJECT_VERSION "0.0.0-0")
         else()
             string(STRIP ${PROJECT_VERSION} PROJECT_VERSION)
-            string(REGEX MATCH "[0-9]*.[0-9]*.[0-9]*(-[0-9]*)" PROJECT_VERSION ${PROJECT_VERSION})
+            message("PROJECT_VERSION = ${PROJECT_VERSION}")
+            string(REGEX MATCH "[0-9]*.[0-9]*.[0-9]*-[0-9]*" PROJECT_VERSION ${PROJECT_VERSION})
         endif()
 
         execute_process(COMMAND ${GIT_EXECUTABLE} show -s --format=%ci
